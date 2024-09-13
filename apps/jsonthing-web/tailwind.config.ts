f1e6164e28ccb1437e32d30393315e53b5ed15c8
lharti <7027812+lharti@users.noreply.github.com>
2024-09-13 11:09:33 +0100

    build(web): add shadcn

--- apps/jsonthing-web/tailwind.config.ts
+++ apps/jsonthing-web/tailwind.config.ts
@@ -1,6 +1,7 @@
 import type { Config } from 'tailwindcss'
 
 const config: Config = {
+    darkMode: ['class'],
     content: [
         './src/pages/**/*.{js,ts,jsx,tsx,mdx}',
         './src/components/**/*.{js,ts,jsx,tsx,mdx}',
@@ -16,8 +17,13 @@ const config: Config = {
                 sans: ['var(--font-geist-sans)'],
                 mono: ['var(--font-geist-mono)'],
             },
+            borderRadius: {
+                lg: 'var(--radius)',
+                md: 'calc(var(--radius) - 2px)',
+                sm: 'calc(var(--radius) - 4px)',
+            },
         },
     },
-    plugins: [],
+    plugins: [require('tailwindcss-animate')],
 }
 export default config
