4316a6e3a0b3545574b04e4c9e404379a00dbdff
lharti <7027812+lharti@users.noreply.github.com>
2024-10-07 12:41:40 +0100

    chore(api): clean and format

--- apps/jsonthing-api/src/config/database.config.ts
+++
@@ -1,9 +0,0 @@
-export interface DatabaseConfig {
-    uri: string
-    name: string
-}
-
-export const databaseConfig = (): DatabaseConfig => ({
-    uri: process.env.DB_URI || '',
-    name: process.env.DB_NAME || '',
-})
