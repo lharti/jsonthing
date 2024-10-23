310c16b5c14c1341228c6f4fda2109c7581a3d78
lharti <7027812+lharti@users.noreply.github.com>
2024-10-23 23:15:03 +0100

    feat(web/doc): update doc route layout

--- apps/jsonthing-web/src/app/docs/[id]/layout.tsx
+++ apps/jsonthing-web/src/app/docs/[id]/layout.tsx
@@ -3,7 +3,13 @@ import React, { PropsWithChildren } from 'react'
 
 const DocLayout: React.FC<PropsWithChildren> = ({ children }) => {
     return (
-        <div className="container mx-auto max-w-screen-md">
+        <div
+            className={`
+              container mx-auto max-w-screen-md px-4
+
+              md:px-2
+            `}
+        >
             <div className="mb-12 flex">
                 <CreateNewDocButton />
             </div>
