22d035a81d30e9d1492162c66752904de345bfe9
lharti <7027812+lharti@users.noreply.github.com>
2024-10-24 20:22:48 +0100

    chore(web): clean app layouts and root page (#32)

--- apps/jsonthing-web/src/app/page.tsx
+++ apps/jsonthing-web/src/app/page.tsx
@@ -1,25 +1,7 @@
-import { DocEditor } from '@/components/DocEditor'
 import React from 'react'
 
 const Home: React.FC = () => {
-    const initialContent = JSON.stringify(
-        {
-            JsonThing: '🚀 in progress JSON thing',
-            features: [
-                '⚡ JSON thing this',
-                '💻 JSON thing that',
-                '🧪 JSON thing everything',
-            ],
-        },
-        null,
-        2,
-    )
-
-    return (
-        <div className="mt-8">
-            <DocEditor initialContent={initialContent} />
-        </div>
-    )
+    return <h1>{'WIP'}</h1>
 }
 
 export default Home
