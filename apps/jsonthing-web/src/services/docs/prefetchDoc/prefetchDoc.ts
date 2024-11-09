7542d6ed47a3fb2ec9e15eb3b43e225a8206c2a7
lharti <7027812+lharti@users.noreply.github.com>
2024-11-09 15:57:10 +0100

    fix(web): calls to internal api instance

--- apps/jsonthing-web/src/services/docs/prefetchDoc/prefetchDoc.ts
+++ apps/jsonthing-web/src/services/docs/prefetchDoc/prefetchDoc.ts
@@ -1,15 +1,18 @@
+'use server'
+
 import { apiClient } from '@/lib/api-client'
 import { QueryClient } from '@tanstack/react-query'
 
-export const prefetchDoc = (id: string, queryClient: QueryClient) =>
-    queryClient.prefetchQuery({
+export const prefetchDoc = async (id: string, queryClient: QueryClient) => {
+    return await queryClient.prefetchQuery({
         queryKey: ['doc', id],
 
         queryFn: () =>
             apiClient
                 .get(`/docs/${id}`, {
-                    baseURL: process.env.NEXT_PUBLIC_INT_API_URL,
+                    baseURL: process.env.INT_API_URL,
                 })
 
                 .then(res => res.data),
     })
+}
